a585d9be2a34e8f69bc80a9d5fbe205943af6069
T-pl <tiagofqu@gmail.com>
2022-08-10 15:08:48 -0300

    config: aws-exports

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
       </header>
+      <div>Teste</div>
       {/* <AmplifySignOut /> */}
     </div>
   );
